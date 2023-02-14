31280284a304768faff2a851965d52aab28e0fea
luigicara <luigi.caravelli98@gmail.com>
2023-02-14 17:57:06 +0100

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <a href="{{ route('home') }}" class="btn">
+             <h1>BEST E-COMMERCE EVER</h1>
+        </a>
+    </div>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MainController;
+
+Route :: get('/', [MainController :: class, 'home'])
+    -> name('home');
+Route :: get('/product', [MainController :: class, 'products'])
+    -> name('product.home');
+
+Route :: get('/product/create', [MainController :: class, 'productCreate'])
+    -> name('product.create');
+Route :: post('/product/create', [MainController :: class, 'productStore'])
+    -> name('product.store');
